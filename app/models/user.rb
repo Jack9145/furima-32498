44f846dce4580706e5b23c4bebd4e16d5d9bc693
Jack9145 <toikakunie@gmail.com>
2020-12-07 16:01:39 +0900

    パスワードへのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
   validates :zenkaku_family_name, presence: true
   validates :kana_first_name, presence: true
   validates :kana_family_name, presence: true
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
