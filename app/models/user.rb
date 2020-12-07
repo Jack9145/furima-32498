a89ae447f4eedf0d230e9699c81b7982a94e54d8
Jack9145 <toikakunie@gmail.com>
2020-12-07 18:17:13 +0900

    バリデーションの設定(名前の入力)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ApplicationRecord
   validates :zenkaku_family_name, presence: true
   validates :kana_first_name, presence: true
   validates :kana_family_name, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
+    validates :zenkaku_first_name
+    validates :zenkaku_family_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'には全角カナ文字を使用してください' } do
+    validates :kana_family_name
+    validates :kana_first_name
+  
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
