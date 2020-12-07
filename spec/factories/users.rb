df1b6a26e4bc3e51e5c70aa7cf05db1f847fa2b8
Jack9145 <toikakunie@gmail.com>
2020-12-07 12:28:31 +0900

    FactoryBot,Faker,Gimeiの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+   Gimei.name
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    zenkaku_first_name    {Gimei.name.first.kanji}
+    zenkaku_family_name   {Gimei.name.last.kanji}
+    kana_first_name       {Gimei.name.first.katakana}
+    kana_family_name      {Gimei.name.last.katakana}
+    birthday              {Faker::Date.backward}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+ RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+   describe 'ユーザーの新規登録' do
+     it "nicknameが空だと登録できない" do
+      
+     end
+     it "emailが空では登録できない" do
+     end
+   end
+ end
