1cb9faada7f0e401ba05a0263c731c68301dcd88
Jack9145 <toikakunie@gmail.com>
2020-12-07 11:25:35 +0900

    ログイン、ログアウト、新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname , presence: true
+  validates :birthday , presence: true
+  validates :zenkaku_first_name, presence: true
+  validates :zenkaku_family_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :kana_family_name, presence: true
+  
 end
