e80e57da9f112bc32095c0fb9c9b521951a4eed0
Jack9145 <toikakunie@gmail.com>
2020-12-08 10:30:25 +0900

    バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname , presence: true
-  validates :birthday , presence: true
-  validates :zenkaku_first_name, presence: true
-  validates :zenkaku_family_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :kana_family_name, presence: true
   
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
-    validates :zenkaku_first_name
-    validates :zenkaku_family_name
+  with_options presence: true do
+    validates :nickname 
+    validates :birthday 
+  
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
+      validates :zenkaku_first_name
+      validates :zenkaku_family_name
+    end
+  
+    with_options format: { with: /\A[ァ-ヴ]+\z/, message: 'には全角カナ文字を使用してください' } do
+      validates :kana_family_name
+      validates :kana_first_name
+    end
+     
+    validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'には英字と数字の両方を含めて設定してください'
   end
-  
-  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'には全角カナ文字を使用してください' } do
-    validates :kana_family_name
-    validates :kana_first_name
-  
-  end
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
+
