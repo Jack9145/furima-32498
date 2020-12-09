48b0ae584d959fd9192c363c3631d23df4e7d5e1
Jack9145 <toikakunie@gmail.com>
2020-12-09 15:37:42 +0900

    販売手数料と販売利益の計算

---
+++ app/javascript/new.js
@@ -0,0 +1,20 @@
+function calculation() {
+  const itemPrice = document.getElementById("item-price")
+  const addTaxPrice = document.getElementById("add-tax-price") 
+  const getProfit = document.getElementById("profit")
+  itemPrice.addEventListener("keyup", (e) => {
+  const priceNow = itemPrice.value
+  const taxPrice = priceNow * 0.1 
+  const fixedTaxPrice = Math.floor(taxPrice)
+  //console.log (taxPrice)
+  addTaxPrice.innerHTML = fixedTaxPrice
+  const profit = priceNow - fixedTaxPrice
+  getProfit.innerHTML = profit})
+};
+window.addEventListener("load", calculation);
+
+
+
+
+
+
