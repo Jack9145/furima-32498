60e8f14969e4e0202487583a5ec4ac6855e6e3d7
Jack9145 <toikakunie@gmail.com>
2020-12-09 11:40:14 +0900

    バリデーションとアソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+ private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :category_id, :shipping_fee_id, :shipping_date_id, :prefecture_id, :status_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+
+  
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: {maximum: 40 ,message: "は４０文字以内で入力してください"}
+    validates :text, length: {maximum: 1000, message: "は1000文字以内で入力して下さい" }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 9999999 ,message: "は¥300〜9,999,999で設定してください（半角数字）"}
+    validates :category_id, numericality: { other_than: 1, message: "を選択してください"}
+    validates :status_id, numericality: { other_than: 1 ,message: "を選択してください"}
+    validates :shipping_fee_id, numericality: { other_than: 1 ,message: "を選択してください"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください"}
+    validates :shipping_date_id, numericality: { other_than: 1, message: "を選択してください"}
+  end
+ 
 end
