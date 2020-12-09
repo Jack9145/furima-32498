017790ba09cc231ef058216a043e61dc185ff980
Jack9145 <toikakunie@gmail.com>
2020-12-09 19:43:31 +0900

    ActiveHashのテストコード編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,33 +32,33 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーが選択されていないと登録できない' do
-        @item.category_id = ''
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank", 'Category is not a number')
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
       it '商品の状態が選択されていないと登録できない' do
-        @item.status_id = ''
+        @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status can't be blank", 'Status is not a number')
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it '配送料の負担が選択されていないと登録できない' do
-        @item.shipping_fee_id = ''
+        @item.shipping_fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping fee can't be blank", 'Shipping fee is not a number')
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
 
       it '発送元の地域が選択されていないと登録できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank", 'Prefecture is not a number')
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it '発送までの日数が選択されてないと登録できない' do
-        @item.shipping_date_id = ''
+        @item.shipping_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping date can't be blank", 'Shipping date is not a number')
+        expect(@item.errors.full_messages).to include("Shipping date must be other than 1")
       end
 
       it '価格が入力されていないと登録できない' do
