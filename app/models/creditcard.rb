cb4d4a736f02810afd014c925f9d9dccf48e49bd
Jack9145 <toikakunie@gmail.com>
2020-12-14 18:09:22 +0900

    フォームオブジェクトの作成

---
+++ app/models/creditcard.rb
@@ -0,0 +1,21 @@
+class Creditcard
+  include ActiveModel::Model
+  attr_accessor :card_type, :expire_month, :expire_year, :security_code, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :card_type
+    validates :expire_month ,numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 12}
+    validates :expire_year, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 99}
+    validates :security_code ,numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 9999 }
+    validates :postal_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid"}
+    validates :address, format: {with:  /\A[ぁ-んァ-ヶ一-龥々0-9-]+\z/, message: "is invalid"}
+    validates :phone_number, format: {with: /\A^(0{1}\d{9,10})$\z/, message: "is invalid"}
+  end
+  validates :building, format: {with: /\A[ぁ-んァ-ヶ一-龥々０-９]+\z/, message: "is invalid"}, allow_blank: true
+
+  def save
+    Shipment.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number )
+  end
+end
